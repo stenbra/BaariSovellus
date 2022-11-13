a9bc027c3ecba853977277b43179a5526fc9dc64
Rasmus Stenberg <rasmus.stenberg@helsinki.fi>
2022-11-13 17:15:26 +0200

    man ive been struggling to get the template files to be recogniced :(

--- Sovellus/app.py
+++ Sovellus/app.py
@@ -1,14 +1,27 @@
 from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///stenbras"
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    return "Heipparalla"
-@app.route("/page1")
-def page1():
-    return "Tämä on sivu 1"
+    result = db.session.execute("SELECT username FROM users")
+    usernames = result.fetchall()
+    return render_template("index.html", count=len(usernames), usernames=usernames) 
 
-@app.route("/page2")
-def page2():
-    return "Tämä on sivu 2"
+@app.route("/newuser")
+def new():
+    return render_template("newuser.html")
+
+@app.route("/send", methods=["POST"])
+def send():
+    uname = request.form["uname"]
+    upass= request.form["upass"]
+    sql = "INSERT INTO messages (username,password) VALUES (:uname,:upass)"
+    db.session.execute(sql, {"uname":uname,"upass":upass})
+    db.session.commit()
+    return redirect("/")
+
