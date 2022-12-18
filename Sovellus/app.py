93fd3b8e53912ad11dad96c6e458d50840041a4a
Rasmus Stenberg <rasmus.stenberg@helsinki.fi>
2022-12-18 18:09:58 +0200

    .env stuff

--- Sovellus/app.py
+++ Sovellus/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from os import getenv
 import os
+from dotenv import  load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")#os.urandom(24)
 
